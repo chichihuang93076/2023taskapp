ea667557fd9b7d6914c552931a5657f5515da8b9
93076 <93076@mail.wzu.edu.tw>
2023-03-14 11:18:30 +0800

    chaper 16 login,session, cookies

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -14,17 +14,19 @@ class Login extends BaseController
   {
     $email = $this->request->getPost('email');
     $password = $this->request->getPost('password');
+    $remember_me = (bool)$this->request->getPost('remember_me');
 
     $auth = service('auth');
 
-    if ($auth->login($email,$password)) {
+    if ($auth->login($email,$password, $remember_me)) {
 
       $redirect_url = session('redirect_url') ?? '/';
 
       unset($_SESSION['redirect_url']);
 
       return redirect()->to($redirect_url)
-                         ->with('info', 'Login successful');
+                         ->with('info', 'Login successful')
+                         ->withCookies();
     } else {
 
       return redirect()->back()
@@ -35,12 +37,10 @@ class Login extends BaseController
 
   public function delete()
   {
-    
-    //$auth = new \App\Libraries\Authentication;
-    //$auth->logout();
     service('auth')->logout();  
 
-    return redirect()->to('/login/showLogoutMessage');
+    return redirect()->to('/login/showLogoutMessage')
+                     ->withCookies();
   }
 
   public function showLogoutMessage()
--- app/Libraries/Authentication.php
+++ app/Libraries/Authentication.php
@@ -7,7 +7,7 @@ class Authentication
  
   Private $user;
  
-  public function login($email, $password)
+  public function login($email, $password, $remember_me)
   {
     $model = new \App\Models\UserModel;    
     
@@ -30,36 +30,111 @@ class Authentication
       return false;
     }
 
-    $session = session();
-    $session->regenerate();        
-    $session->set("user_id", $user->id);
+    $this->logInUser($user);
+
+    if ($remember_me) {
+      
+      $this->rememberLogin($user->id);
+
+    }
 
     return true;
   }
 
+  private function logInUser($user)
+  {
+    $session = session();
+    $session->regenerate();        
+    $session->set("user_id", $user->id);
+  }
+
+  private function rememberLogin($user_id)
+  {
+    $model = new \App\Models\RememberedLoginModel;
+                             
+    list($token, $expiry) = $model->rememberUserLogin($user_id);
+
+    $response = service('response');
+
+    $response->setCookie('remember_me', $token, $expiry);
+
+  }
+
   public function logout()
   {
+    $token = service('request')->getCookie('remember_me');
+
+    if ($token !== null) {
+
+      $model = new  \App\Models\RememberedLoginModel;
+
+      $model->deleteByToken($token);
+    }
+    
+    service('response')->deleteCookie('remember_me');
+
     session()->destroy();
+  }
+
+
+  private function getUserFromSession()
+  {
+    if ( ! session()->has('user_id') ) {
+      return null;
+    }
+
+    $model = new \App\Models\UserModel;
+
+    $user = $model->find(session()->get('user_id'));
+
+    if ($user && $user->is_active)
+    {
+      return $user;
+    }
+  }
+
+  private function getUserFromRememberCookie()
+  {
+    $request = service('request');
+
+    $token = $request->getCookie('remember_me');
+
+    if ($token === null) {
+
+      return null;
+    }
+
+    $remembered_login_model = new \App\Models\RememberedLoginModel;
+
+    $remembered_login = $remembered_login_model->findByToken($token);
+
+    if ($remembered_login === null) {
+
+      return null;
+    }
+
+    $user_model= new \App\Models\UserModel;
+
+    $user = $user_model->find($remembered_login['user_id']);
+
+    if ($user && $user->is_active) {
+
+      $this->logInUser($user);
+
+      return $user;
+    }
   }
 
   public function getCurrentUser()
   {
-    if ( !session()->has('user_id')) {
-            
-      return null;
-      
-    }
-  
     if ($this->user === null) {
 
-      $model = new \App\Models\UserModel;
+      $this->user = $this->getUserFromSession();
+    }
 
-      $user = $model->find(session()->get('user_id'));
+    if ($this->user === null) {
 
-      if ($user && $user->is_active)
-      {
-        $this->user = $user;
-      }
+      $this->user = $this->getUserFromRememberCookie();
     }    
 
     return $this->user;
